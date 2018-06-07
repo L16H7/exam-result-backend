69a4b8d9b510c6e7a4aa795005458e5c559ba160
lightMK <linmyatko@hotmail.com>
2018-06-07 20:38:58 +0630

    change time table on school year select

--- client/src/components/timeTable/timeTable.js
+++ client/src/components/timeTable/timeTable.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
-import {Icon, Table} from 'semantic-ui-react';
+import { Container, Icon, Select, Table } from 'semantic-ui-react';
 
 import { getTimeTable } from '../../actions';
+import { schoolYears } from '../../constants/schoolYears';
 import { connect } from 'react-redux';
 import { bindActionCreators } from "redux";
 import timeTable from '../../reducers/timeTable'
 
 class TimeTable extends Component {
+  constructor(props) {
+    super(props);
+    this.changeSchoolYear = this.changeSchoolYear.bind(this);
+  }
+
   componentDidMount() {
     this.props.getTimeTable(1);
   } 
+
+  changeSchoolYear(e, data) {
+    this.props.getTimeTable(data.value);
+  }
 
   renderPeriods(periods) {
     let tableCells = periods.map(period => <Table.Cell key={period.id}><b>{period.subject}</b></Table.Cell>);
@@ -17,56 +27,61 @@ class TimeTable extends Component {
   }
 
   render() {
-    if (!this.props.timeTable.length) return (<div></div>);
+    if (!this.props.timeTable.length) return (
+      <Select placeholder='Select school year' options={schoolYears} onChange={this.changeSchoolYear} />
+    );
 
     return (
-      <Table celled structured>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell rowSpan='2'>Day/Time</Table.HeaderCell>
-          <Table.HeaderCell rowSpan='2'>Period-1<br/>8:30-9:30 am</Table.HeaderCell>
-          <Table.HeaderCell rowSpan='2'>Period-2<br/>9:35-10:35 am</Table.HeaderCell>
-          <Table.HeaderCell rowSpan='2'>Period-3<br/>10:40-11:40 am</Table.HeaderCell>
-          <Table.HeaderCell rowSpan='5'></Table.HeaderCell>
-          <Table.HeaderCell rowSpan='2'>Period-4<br/>8:30-9:30 am</Table.HeaderCell>
-          <Table.HeaderCell rowSpan='2'>Period-5<br/>9:35-10:35 am</Table.HeaderCell>
-          <Table.HeaderCell rowSpan='2'>Period-6<br/>10:40-11:40 am</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-  
-      <Table.Body>
+      <Container>
+        <Select placeholder='Select school year' options={schoolYears} onChange={this.changeSchoolYear} />
+        <Table celled structured>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell rowSpan='2'>Day/Time</Table.HeaderCell>
+            <Table.HeaderCell rowSpan='2'>Period-1<br/>8:30-9:30 am</Table.HeaderCell>
+            <Table.HeaderCell rowSpan='2'>Period-2<br/>9:35-10:35 am</Table.HeaderCell>
+            <Table.HeaderCell rowSpan='2'>Period-3<br/>10:40-11:40 am</Table.HeaderCell>
+            <Table.HeaderCell rowSpan='5'></Table.HeaderCell>
+            <Table.HeaderCell rowSpan='2'>Period-4<br/>8:30-9:30 am</Table.HeaderCell>
+            <Table.HeaderCell rowSpan='2'>Period-5<br/>9:35-10:35 am</Table.HeaderCell>
+            <Table.HeaderCell rowSpan='2'>Period-6<br/>10:40-11:40 am</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+    
+        <Table.Body>
 
-        <Table.Row>
-          <Table.Cell></Table.Cell>
-          <Table.Cell></Table.Cell>
-          <Table.Cell></Table.Cell>
-          <Table.Cell></Table.Cell>
-          <Table.Cell  rowSpan='6' ><div className='vertical_text'>Lunch Break (11:40 - 12:40 pm)</div> </Table.Cell>
-          <Table.Cell></Table.Cell>
-          <Table.Cell></Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Monday</Table.Cell>
-          {this.renderPeriods(this.props.timeTable.filter(e => e.day == 1))}
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Tuesday</Table.Cell>
-          {this.renderPeriods(this.props.timeTable.filter(e => e.day == 2))}
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Wednesday</Table.Cell>
-          {this.renderPeriods(this.props.timeTable.filter(e => e.day == 3))}
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Thursday</Table.Cell>
-          {this.renderPeriods(this.props.timeTable.filter(e => e.day == 4))}
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Friday</Table.Cell>
-          {this.renderPeriods(this.props.timeTable.filter(e => e.day == 5))}
-        </Table.Row>
-      </Table.Body>
-    </Table>
+          <Table.Row>
+            <Table.Cell></Table.Cell>
+            <Table.Cell></Table.Cell>
+            <Table.Cell></Table.Cell>
+            <Table.Cell></Table.Cell>
+            <Table.Cell  rowSpan='6' ><div className='vertical_text'>Lunch Break (11:40 - 12:40 pm)</div> </Table.Cell>
+            <Table.Cell></Table.Cell>
+            <Table.Cell></Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Monday</Table.Cell>
+            {this.renderPeriods(this.props.timeTable.filter(e => e.day == 1))}
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Tuesday</Table.Cell>
+            {this.renderPeriods(this.props.timeTable.filter(e => e.day == 2))}
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Wednesday</Table.Cell>
+            {this.renderPeriods(this.props.timeTable.filter(e => e.day == 3))}
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Thursday</Table.Cell>
+            {this.renderPeriods(this.props.timeTable.filter(e => e.day == 4))}
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Friday</Table.Cell>
+            {this.renderPeriods(this.props.timeTable.filter(e => e.day == 5))}
+          </Table.Row>
+        </Table.Body>
+        </Table>
+      </Container>
     );
   }
 }
---
+++ client/src/constants/schoolYears.js
@@ -0,0 +1,7 @@
+export const schoolYears = [
+  { key: '1', value: 1, text: 'I BE EC' },
+  { key: '2', value: 2, text: 'II BE EC' },
+  { key: '3', value: 3, text: 'III BE EC' },
+  { key: '4', value: 4, text: 'IV BE EC' },
+  { key: '5', value: 5, text: 'V BE EC' }
+];
