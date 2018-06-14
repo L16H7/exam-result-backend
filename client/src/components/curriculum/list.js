593fc026a4e7ad93423312811222bd0930b3534a
lightMK <linmyatko@hotmail.com>
2018-06-14 10:00:01 +0630

    semantic grid system added

--- client/src/components/curriculum/list.js
+++ client/src/components/curriculum/list.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
+import { Grid } from 'semantic-ui-react';
 import CurriculumCard from './card';
 
 class CurriculumList extends Component {
   render() {
     return (
-      <CurriculumCard curriculumDate={'2018-06-23'} period={'Period 1'} subject={'Chemistry'} description={'Test Description'} />
+      <Grid>
+        <Grid.Row columns={3}>
+          <Grid.Column>
+            <CurriculumCard curriculumDate={'2018-06-23'} period={'Period 1'} subject={'Chemistry'} 
+              description={'Test Test Test Test Test Test Test Test Test Test Description'} />
+          </Grid.Column>
+          <Grid.Column> 
+            <CurriculumCard curriculumDate={'2018-06-23'} period={'Period 1'} subject={'Chemistry'} description={'Test Description'} />
+          </Grid.Column>
+          <Grid.Column>
+            <CurriculumCard curriculumDate={'2018-06-23'} period={'Period 1'} subject={'Chemistry'} description={'Test Description'} />
+          </Grid.Column>
+        </Grid.Row>
+        <CurriculumCard curriculumDate={'2018-06-23'} period={'Period 1'} subject={'Chemistry'} description={'Test Description'} />
+        <CurriculumCard curriculumDate={'2018-06-23'} period={'Period 1'} subject={'Chemistry'} description={'Test Description'} />
+      </Grid>
     );
   }
 }
