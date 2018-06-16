302139bf9f91806d2a46fc3ded557726108cbb94
lightMK <linmyatko@hotmail.com>
2018-06-16 22:03:11 +0630

    form input changes handled

--- client/src/components/curriculum/edit.js
+++ client/src/components/curriculum/edit.js
@@ -1,34 +1,73 @@
 import React, { Component } from 'react';
-import { Button, Checkbox, Form } from 'semantic-ui-react';
+import { Button, Checkbox, Input, Form, Label } from 'semantic-ui-react';
 
 import { getCurriculumById } from '../../actions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 class CurriculumEdit extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      curriculum: {
+        curriculumDate: '2018-06-30',
+        period: 0,
+        subject: '',
+        description: ''
+      }
+    }
+  }
+
   componentDidMount() {
     this.props.getCurriculumById(this.props.id);
   }
+
+  componentWillReceiveProps(props) {
+    let newCurriculum = props.curriculum[0];
+    newCurriculum.curriculumDate = newCurriculum.curriculumDate.slice(0, 10);
+    
+    console.log(newCurriculum);
+
+    this.setState({ curriculum: newCurriculum });
+  }
   
+  handleChange = (name) => (event) => {
+    const { curriculum } = this.state;
+    const newCurriculum = {
+      ...curriculum,
+      [name]: event.target.value
+    };
+
+    this.setState({ curriculum: newCurriculum });
+  }
+
   render() {
     if (!this.props.curriculum.length) {
       return (
         <h1>ERROR 404</h1>
       );
     }
+
     return (
       <Form>
         <Form.Field>
-          <label>First Name</label>
-          <input placeholder='First Name' />
+          <Label>Date</Label>
+          <input type='date' value={this.state.curriculum.curriculumDate} onChange={this.handleChange('curriculumDate')} />
         </Form.Field>
         <Form.Field>
-          <label>Last Name</label>
-          <input placeholder='Last Name' />
+          <Label>Period</Label>
+          <Input value={this.state.curriculum.period} onChange={this.handleChange('period')} />
         </Form.Field>
         <Form.Field>
-          <Checkbox label='I agree to the Terms and Conditions' />
+          <Label>Subject</Label>
+          <Input value={this.state.curriculum.subject} onChange={this.handleChange('subject')} />
         </Form.Field>
+        <Form.Field>
+          <Label>Description</Label>
+          <Input value={this.state.curriculum.description} onChange={this.handleChange('description')} />
+        </Form.Field>
+
         <Button type='submit'>Submit</Button>
       </Form>
     );
